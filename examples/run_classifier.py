b406d61bb7cb38b027601190105074921d2c1155
Mihir S. Kale <mihirkale.s@BPW-C02PJ2RWFVH5.fios-router.home>
2018-11-21 21:42:01 -0500

    modified eval to write predictions

--- examples/run_classifier.py
+++ examples/run_classifier.py
@@ -242,12 +242,16 @@ class ColaProcessor(DataProcessor):
         return examples
 
 
-def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer):
-    """Loads a data file into a list of `InputBatch`s."""
-
+def create_label_map(label_list):
     label_map = {}
     for (i, label) in enumerate(label_list):
         label_map[label] = i
+    return label_map
+
+def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer):
+    """Loads a data file into a list of `InputBatch`s."""
+
+    label_map = create_label_map(label_list)
 
     features = []
     for (ex_index, example) in enumerate(examples):
@@ -430,9 +434,13 @@ def main():
                         type=int,
                         help="Total batch size for training.")
     parser.add_argument("--eval_batch_size",
-                        default=8,
+                        default=32,
                         type=int,
                         help="Total batch size for eval.")
+    parser.add_argument("--eval_every",
+                        default=1000,
+                        type=int,
+                        help="evaluate at every eval_every number of steps ")
     parser.add_argument("--learning_rate",
                         default=5e-5,
                         type=float,
@@ -562,6 +570,7 @@ def main():
                          warmup=args.warmup_proportion,
                          t_total=num_train_steps)
 
+    metadata = {}
     global_step = 0
     if args.do_train:
         train_features = convert_examples_to_features(
@@ -619,61 +628,84 @@ def main():
                         optimizer.step()
                     model.zero_grad()
                     global_step += 1
+                    metadata['global_step'] = global_step,
+                    metadata['tr_loss'] = tr_loss
+                    metadata['nb_tr_steps'] = nb_tr_steps
+                    if nb_tr_steps % eval_every == 0 :
+                        evaluate(args, model, processor, tokenizer, metadata, device)
+
 
     if args.do_eval:
-        eval_examples = processor.get_dev_examples(args.data_dir)
-        eval_features = convert_examples_to_features(
-            eval_examples, label_list, args.max_seq_length, tokenizer)
-        logger.info("***** Running evaluation *****")
-        logger.info("  Num examples = %d", len(eval_examples))
-        logger.info("  Batch size = %d", args.eval_batch_size)
-        all_input_ids = torch.tensor([f.input_ids for f in eval_features], dtype=torch.long)
-        all_input_mask = torch.tensor([f.input_mask for f in eval_features], dtype=torch.long)
-        all_segment_ids = torch.tensor([f.segment_ids for f in eval_features], dtype=torch.long)
-        all_label_ids = torch.tensor([f.label_id for f in eval_features], dtype=torch.long)
-        eval_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
-        if args.local_rank == -1:
-            eval_sampler = SequentialSampler(eval_data)
-        else:
-            eval_sampler = DistributedSampler(eval_data)
-        eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=args.eval_batch_size)
+        evaluate(args, model, processor, tokenizer,metadata,device)
 
-        model.eval()
-        eval_loss, eval_accuracy = 0, 0
-        nb_eval_steps, nb_eval_examples = 0, 0
-        for input_ids, input_mask, segment_ids, label_ids in eval_dataloader:
-            input_ids = input_ids.to(device)
-            input_mask = input_mask.to(device)
-            segment_ids = segment_ids.to(device)
-            label_ids = label_ids.to(device)
 
-            with torch.no_grad():
-                tmp_eval_loss, logits = model(input_ids, segment_ids, input_mask, label_ids)
+def evaluate(args,model,processor,tokenizer,metadata,device):
+    label_list = processor.get_labels()
+    label_map = create_label_map(label_list)
+    id2label = {idx: label for label, idx in label_map.items()}
+    eval_examples = processor.get_dev_examples(args.data_dir)
+    eval_features = convert_examples_to_features(
+        eval_examples, label_list, args.max_seq_length, tokenizer)
+    logger.info("***** Running evaluation *****")
+    logger.info("  Num examples = %d", len(eval_examples))
+    logger.info("  Batch size = %d", args.eval_batch_size)
+    all_input_ids = torch.tensor([f.input_ids for f in eval_features], dtype=torch.long)
+    all_input_mask = torch.tensor([f.input_mask for f in eval_features], dtype=torch.long)
+    all_segment_ids = torch.tensor([f.segment_ids for f in eval_features], dtype=torch.long)
+    all_label_ids = torch.tensor([f.label_id for f in eval_features], dtype=torch.long)
+    eval_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+    if args.local_rank == -1:
+        eval_sampler = SequentialSampler(eval_data)
+    else:
+        eval_sampler = DistributedSampler(eval_data)
+    eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=args.eval_batch_size)
 
-            logits = logits.detach().cpu().numpy()
-            label_ids = label_ids.to('cpu').numpy()
-            tmp_eval_accuracy = accuracy(logits, label_ids)
+    model.eval()
+    eval_loss, eval_accuracy = 0, 0
+    nb_eval_steps, nb_eval_examples = 0, 0
+    nb_tr_steps = metadata['nb_tr_steps']
+    output_predictions_path = os.path.join(args.output_dir, "eval_predictions_{0}.txt".format(str(nb_tr_steps)))
+    output_predictions_file = open(output_predictions_path,"w")
+    output_predictions_file.write("Id,Category\n")
+    for input_ids, input_mask, segment_ids, label_ids in eval_dataloader:
+        input_ids = input_ids.to(device)
+        input_mask = input_mask.to(device)
+        segment_ids = segment_ids.to(device)
+        label_ids = label_ids.to(device)
 
-            eval_loss += tmp_eval_loss.mean().item()
-            eval_accuracy += tmp_eval_accuracy
+        with torch.no_grad():
+            tmp_eval_loss, logits = model(input_ids, segment_ids, input_mask, label_ids)
 
-            nb_eval_examples += input_ids.size(0)
-            nb_eval_steps += 1
+        logits = logits.detach().cpu().numpy()
+        label_ids = label_ids.to('cpu').numpy()
+        tmp_eval_accuracy = accuracy(logits, label_ids)
 
-        eval_loss = eval_loss / nb_eval_steps
-        eval_accuracy = eval_accuracy / nb_eval_examples
+        for idx, input_id in enumerate(input_ids):
+            text = ",".join([str(input_id), id2label[label_ids[idx]]]) + "\n"
+            output_predictions_file.write(text)
 
-        result = {'eval_loss': eval_loss,
-                  'eval_accuracy': eval_accuracy,
-                  'global_step': global_step,
-                  'loss': tr_loss/nb_tr_steps}
+        eval_loss += tmp_eval_loss.mean().item()
+        eval_accuracy += tmp_eval_accuracy
 
-        output_eval_file = os.path.join(args.output_dir, "eval_results.txt")
-        with open(output_eval_file, "w") as writer:
-            logger.info("***** Eval results *****")
-            for key in sorted(result.keys()):
-                logger.info("  %s = %s", key, str(result[key]))
-                writer.write("%s = %s\n" % (key, str(result[key])))
+        nb_eval_examples += input_ids.size(0)
+        nb_eval_steps += 1
+
+    eval_loss = eval_loss / nb_eval_steps
+    eval_accuracy = eval_accuracy / nb_eval_examples
+
+    result = {'eval_loss': eval_loss,
+              'eval_accuracy': eval_accuracy,
+              'global_step': metadata['global_step'],
+              'loss': metadata['tr_loss'] / metadata['nb_tr_steps']}
+
+
+    output_eval_file = os.path.join(args.output_dir, "eval_results_{0}.txt".format(str(nb_tr_steps)))
+    with open(output_eval_file, "w") as writer:
+        logger.info("***** Eval results *****")
+        for key in sorted(result.keys()):
+            logger.info("  %s = %s", key, str(result[key]))
+            writer.write("%s = %s\n" % (key, str(result[key])))
+
 
 if __name__ == "__main__":
     main()
